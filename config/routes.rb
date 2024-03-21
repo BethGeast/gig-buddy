2441444c37be4e2dfcd9b92e4a58deee165547ec
beth geast <beth.geast@gmail.com>
2024-03-21 13:54:29 +0000

    matches controller continued

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :profiles, except: %i[index destroy]
+  resources :profiles, except: %i[index destroy] do
+    resources :matches, only: :create
+  end
   delete '/profiles/:id', to: 'profiles#destroy', as: :delete_profile
   resources :selected_artists, only: %i[new create]
   resources :selected_languages, only: %i[new create]
-  resources :matches, only: :index
+  resources :matches, only: %i[index]
 end
