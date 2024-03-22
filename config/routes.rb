ae1b59890b32b7cfc507fb2664d049cd7df96665
Simone Napoli <sn.simonenapoli@gmail.com>
2024-03-22 11:42:35 +0100

    completed search for spotify-api frontandback

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'artists/index'
   devise_for :users
   root to: "pages#home"
   resources :profiles, except: %i[index destroy]
   delete '/profiles/:id', to: 'profiles#destroy', as: :delete_profile
   resources :selected_artists, only: %i[new create]
+  resources :artists, only: :index
 end
