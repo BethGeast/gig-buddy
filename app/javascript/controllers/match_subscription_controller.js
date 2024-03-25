201c02659ca1f6fbdb7fa9afe0a9a543675ca6f1
beth geast <beth.geast@gmail.com>
2024-03-25 20:04:31 +0000

    changes chat to display left and right depending on sender or receiver

--- app/javascript/controllers/match_subscription_controller.js
+++ app/javascript/controllers/match_subscription_controller.js
@@ -3,7 +3,7 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="match-subscription"
 export default class extends Controller {
-  static values = { matchId: Number }
+  static values = { matchId: Number, currentUserId: Number }
   static targets = ["messages"]
 
   connect() {
@@ -14,8 +14,29 @@ export default class extends Controller {
     console.log(`Subscribe to the match chatroom with the id ${this.matchIdValue}.`)
   }
 
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
